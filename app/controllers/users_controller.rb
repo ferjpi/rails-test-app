50db51952b69016fd833a6d8cd2f7882645cb5ea
ferjpi <fernandojpt1203@gmail.com>
2021-01-31 14:41:14 -0500

    cuestionario home ruby

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def ruby
+  end
+
+  def git
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'users#index'
+  get '/basics', to: 'users#index'
+  get '/ruby', to: 'users#ruby'
+  get '/git', to: 'users#git'
 end
