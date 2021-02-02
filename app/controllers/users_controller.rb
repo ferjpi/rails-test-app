94802fea0f50150a76a4e61b9efd37c1411b3983
ferjpi <fernandojpt1203@gmail.com>
2021-02-01 21:03:11 -0500

    latest changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,20 @@ class UsersController < ApplicationController
   def index
   end
 
+  def basics
+    @time = time
+  end
+  
   def ruby
+    @time = time
   end
 
   def git
+    @time = time
+  end
+
+  private
+  def time
+    Time.now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'users#index'
-  get '/basics', to: 'users#index'
+  get '/basics', to: 'users#basics'
   get '/ruby', to: 'users#ruby'
   get '/git', to: 'users#git'
 end
